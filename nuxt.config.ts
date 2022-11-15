42c4b514f442ea260293a98efcbafda2be27c0cf
Tanya Jain <tanya.jain27may@gmail.com>
2022-11-14 18:05:25 -0800

    style(core): setup base template styles
    Signed-off-by: Tanya Jain <tanya.jain27may@gmail.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,23 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-
+import svgLoader from 'vite-svg-loader'
 
 export default defineNuxtConfig({
+  css: [
+    '~/assets/styles/main.scss',
+    '~/assets/styles/fluid_typography.scss',
+    '~/assets/styles/typography.scss',
+    '~/assets/styles/grid.scss',
+    '~/assets/styles/buttons.scss',
+    '~/assets/styles/inputs.scss'
+],
   vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/styles/_colors.scss" as *;'
+        }
+      }
+    },
+    plugins: [svgLoader()],
   }
 })
