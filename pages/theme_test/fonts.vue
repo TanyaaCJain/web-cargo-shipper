2a316a4ebe6a82a886e5f5094075d918ee4f3b7f
Tanya Jain <tanya.jain27may@gmail.com>
2022-11-14 19:21:03 -0800

    feat(theme): add test page for theme styles
    Signed-off-by: Tanya Jain <tanya.jain27may@gmail.com>

---
+++ pages/theme_test/fonts.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="arrow-grid">
+        <div class="arrow-grid-child-fw">
+            <h1>H1  Rubik: where are them jovel Vtol at</h1>
+            <h2>H2 Rubik: where are them jovel Vtol at</h2>
+            <h3>H3 Rubik: where are them jovel Vtol at</h3>
+            <h4>H4 Rubik: where are them jovel Vtol at</h4>
+            <p>
+                Facilisis volutpat elit eiusmod vulputate do sodales eleifend venenatis duis proin 
+                lacinia non. Odio mollis etiam tempor ultricies etiam auctor fringilla velit sus-pendisse 
+                lectus quam molestie magna.
+            </p>
+            <a href="#">Link Type</a>
+            <div class="primary-btn-light-bg primary-btn-lg">
+                <div class="btn-small">Button text small</div>
+                <div class="btn-medium">Button text medium</div>
+                <div class="btn-large">Button text large</div>
+            </div>
+            <br>
+            <div class="primary-btn-light-bg primary-btn-lg">
+                <div class="field-title">Field title</div>
+                <div class="field-info">Field text</div>
+                <div class="input-typed">Input text</div>
+                <div class="input-placeholder">Input placeholder</div>
+            </div>
+        </div>
+    </div>
+</template>
