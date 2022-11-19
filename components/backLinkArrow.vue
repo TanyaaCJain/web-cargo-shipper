ae00e49ad559e2a507ea2507bfe6d2caff1f63b1
Tanya Jain <tanya.jain27may@gmail.com>
2022-11-18 17:45:06 -0800

    feat(component): add action button and back link arrow components
    Signed-off-by: Tanya Jain <tanya.jain27may@gmail.com>

---
+++ components/backLinkArrow.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="app__back-icon" @click="goBack">
+        <img :src="`/_nuxt/assets/icons/icon_back_arrow.svg`" />
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        goBack() {
+            window.history.length > 1
+                ? this.$router.back()
+                : this.$router.push('/')
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.app__back-icon {
+    width: 20px;
+    padding-bottom: 30.5px;
+}
+</style>
