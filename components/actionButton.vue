f8edf20f7c3ed5912688e5bf3fe438810fbc0031
Tanya Jain <tanya.jain27may@gmail.com>
2022-11-18 17:45:06 -0800

    feat(component): add action button and back link arrow components
    Signed-off-by: Tanya Jain <tanya.jain27may@gmail.com>

---
+++ components/actionButton.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="button">
+        <button :class="['button__submit','button', buttonTypeClass, buttonBgClass, darkMode ? 'button-dark-mode' : 'button-light-mode']" @click="buttonAction">
+            <div class="button__submit__text">
+                {{ buttonText }}
+            </div>
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        darkMode: {
+            type: Boolean,
+            default: false
+        },
+        buttonText: {
+            type: String,
+            required: true
+        },
+        buttonTypeClass: {
+            type: String,
+            required: true
+        },
+        buttonBgClass: {
+            type: String,
+            required: true
+        },
+        buttonAction: {
+            type: Function,
+            required: true
+        }
+    },
+}
+</script>
+
+<style scoped lang="scss">
+.button__submit {
+    width: 100%;
+}
+</style>
