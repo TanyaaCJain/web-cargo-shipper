1ca7d4b17f7005752ab10faffbc3e799467938e1
Tanya Jain <tanya.jain27may@gmail.com>
2022-12-20 18:44:03 -0800

    feat(page): add package specification page
    Signed-off-by: Tanya Jain <tanya.jain27may@gmail.com>

---
+++ components/commuteLocation.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="commute-location">
+        <h2 class="start">
+            {{ departureLocation }}
+        </h2>
+        <div class="to-arrow">
+            <img :src="`/_nuxt/assets/icons/icon_drop_right.svg`" />
+        </div>
+        <h2 class="end">
+            {{ arrivalLocation }}
+        </h2>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        departureLocation: {
+            type: String,
+            required: true
+        },
+        arrivalLocation: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style  scoped lang="scss">
+.commute-location {
+    display: grid;
+    grid-template-columns: 1fr auto 1fr;
+    grid-column-gap: 16px;
+
+    .start {
+        text-align: center;
+    }
+
+    .to-arrow {
+        width: 20px;
+    }
+
+    .end {
+        text-align: center;
+    }
+}
+</style>
