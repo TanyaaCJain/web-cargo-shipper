4d5407dff4b7bc3d184466f1dc3add24021fd78e
Tanya Jain <tanya.jain27may@gmail.com>
2023-01-11 19:01:31 +0530

    feat(page): add flight map-display page
    Signed-off-by: Tanya Jain <tanya.jain27may@gmail.com>

---
+++ pages/flights.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="arrow-light-blue-bg mapper-container">
+        <div class="arrow-cargo-body">
+            <div class="arrow-grid white-bg-header">
+                <div class="arrow-grid-child-fw specify-package">
+                    <BackLinkArrow/>
+                    <CommuteLocation/>
+                </div>
+            </div>
+            <MapRoute />
+            <div class="commute-card-background">
+                <CommuteCard />
+            </div>
+        </div>
+        <CargoStepper :step="2" />
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+.mapper-container {
+    min-height: 100vh;
+
+    .arrow-cargo-body {
+        margin-bottom: 120px;
+        .commute-card-background {
+            // background: $greys_blue_extra_light_muted;
+            padding: 20px;
+            height: 100%;
+        }
+    }
+}
+</style>
