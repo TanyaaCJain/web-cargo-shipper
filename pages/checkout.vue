a89503a4d3ad85975f5a5f4f45a353d0cab767e9
Tanya Jain <tanya.jain27may@gmail.com>
2023-01-13 13:16:19 +0530

    feat(page): add checkout page
    Signed-off-by: Tanya Jain <tanya.jain27may@gmail.com>

---
+++ pages/checkout.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="arrow-light-blue-bg checkout-container">     
+        <div class="arrow-grid">
+            <div class="arrow-grid-child-fw">
+                <h2>Cargo flights checkout</h2>
+                <h3> Flight 1</h3>
+                <p>1 package</p>
+                <CommuteCard />
+                <div class="add-destination">
+                    <ActionButton
+                        :buttonText="'Add new destination'"
+                        :buttonTextClass="'btn-medium'"
+                        :buttonTypeClass="'secondary-btn-sm'"
+                        :buttonBgClass="'secondary-btn-light-bg'"
+                        :buttonIcon="'add'"
+                        :buttonAction="addDestination"
+                    />
+                </div>
+            </div>
+        </div>
+        <CargoStepper :step="3" />
+    </div>
+</template>
+
+<script setup>
+const addDestination = () => {
+    $router.push('/checkout')
+};
+</script>
+
+<style lang="scss" scoped>
+.checkout-container {
+    min-height: 100vh;
+
+    .arrow-grid {
+        margin-bottom: 120px;
+
+        .add-destination {
+            margin-top: 20px;
+            max-width: 300px;
+        }
+    }
+}
+</style>
